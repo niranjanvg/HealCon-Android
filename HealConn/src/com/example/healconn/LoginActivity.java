0620d78191a499ba880dc602d777675f100da511
Wenting Shi <alexshi1991@gmail.com>
2014-04-10 21:31:40 -0400

    Fixed app name

--- HealConn/src/com/example/healconn/LoginActivity.java
+++ HealConn/src/com/example/healconn/LoginActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
